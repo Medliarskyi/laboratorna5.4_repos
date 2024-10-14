3e9e2708384c496e15fc075a862d6731ea2f81d5
Medliarskyi <med.uras111@gmail.com>
2024-10-14 16:28:08 +0300

    first master commit

---
+++ UnitTest54/UnitTest54.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab54/lab54.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest54
+{
+	TEST_CLASS(UnitTest54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double result = S0(1, 5);
+			Assert::AreEqual(result, 1.27008, 0.000001);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			double result = S1(1, 5, 1);
+			Assert::AreEqual(result, 1.27008, 0.000001);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			double result = S2(1, 5, 5);
+			Assert::AreEqual(result, 1.27008, 0.000001);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			double result = S3(1, 5, 1, 1);
+			Assert::AreEqual(result, 1.27008, 0.000001);
+		}
+		TEST_METHOD(TestMethod5)
+		{
+			double result = S4(1, 5, 5, 1);
+			Assert::AreEqual(result, 1.27008, 0.000001);
+		}
+	};
+}
---
+++ lab54/lab54.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Ітераційний спосіб
+double S0(const int K, const int N)
+{
+    double s = 1;
+    for (int i = K; i <= N; i++)
+        s *= (K * 1.0 / i) + (i * 1.0 / N);
+    return s;
+}
+
+// Рекурсивний спуск, параметр зростає
+double S1(const int K, const int N, const int i)
+{
+    if (i > N)
+        return 1.0;
+    else
+        return ((K * 1.0 / i) + (i * 1.0 / N)) * S1(K, N, i + 1);
+}
+
+// Рекурсивний спуск, параметр спадає
+double S2(const int K, const int N, const int i)
+{
+    if (i < K)
+        return 1.0;
+    else
+        return ((K * 1.0 / i) + (i * 1.0 / N)) * S2(K, N, i - 1);
+}
+
+// Рекурсивний підйом, параметр зростає
+double S3(const int K, const int N, const int i, double t)
+{
+    t *= (K * 1.0 / i) + (i * 1.0 / N);
+    if (i >= N)
+        return t;
+    else
+        return S3(K, N, i + 1, t);
+}
+
+// Рекурсивний підйом, параметр спадає
+double S4(const int K, const int N, const int i, double t)
+{
+    t *= (K * 1.0 / i) + (i * 1.0 / N);
+    if (i <= K)
+        return t;
+    else
+        return S4(K, N, i - 1, t);
+}
+
+int main()
+{
+    int K, N;
+    cout << "K = "; cin >> K;
+    cout << "N = "; cin >> N;
+
+    cout << "(iter) S0 = " << S0(K, N) << endl;
+    cout << "(rec up ++) S1 = " << S1(K, N, K) << endl;
+    cout << "(rec up --) S2 = " << S2(K, N, N) << endl;
+    cout << "(rec down ++) S3 = " << S3(K, N, K, 1) << endl;
+    cout << "(rec down --) S4 = " << S4(K, N, N, 1) << endl;
+
+    return 0;
+}
